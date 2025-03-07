b9bfc782e84c11e938ed4f6c1d115daeca21401a
Hemant Karekar <hemant.v.karekar@gmail.com>
2025-03-07 15:19:13 +0530

    Load Wireframe & Templates

---
+++ mandi-admin/application/views/layout/_2_mini.php
@@ -0,0 +1,12 @@
+<!-- partial:partials/_footer.html -->
+<?php //$this->load->view('components/partials/_footer'); ?>
+<!-- partial -->
+
+</div>
+</div>
+
+<?php $this->load->view('components/partials/_js') ?>
+
+</body>
+
+</html>
